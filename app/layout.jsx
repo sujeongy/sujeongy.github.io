f5d01932d43fbbf6cccb7286c8e14e03ae85b8ab
sujeongy <sujeongy.dev@gmail.com>
2026-01-13 11:45:33 +0900

    fix: correct repository base URL by adding blob/main path

--- app/layout.jsx
+++ app/layout.jsx
@@ -51,7 +51,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase='https://github.com/sujeongy/sujeongy.github.io'
+          docsRepositoryBase='https://github.com/sujeongy/sujeongy.github.io/blob/main'
           footer={footer}
           // ... Your additional layout options
         >
